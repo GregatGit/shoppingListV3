eeccec05e497cc3d525f7a37305d1419d8027f7c
Greg Duncan <gregdd@outlook.com>
2017-07-24 13:01:24 +1000

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-"# shoppingListV3"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/GregatGit/shoppingListV3.git git push -u origin master
 "# shoppingListV3" 
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const srcDir = resolve(__dirname, 'src')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  devtool: "source-map",
+  module: {
+    rules: [ {
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'standard-loader',
+      exclude: /node_modules/
+    },{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    },{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${srcDir}/index.html`
+    }),
+    new DashboardPlugin()
+  ]
+}
